f4784a259ccf87ac69f45bd021464bb6440adcf6
suttang <suttang@gmail.com>
2019-12-23 20:44:51 +0900

    fix metrics

--- lmnet/lmnet/networks/super_resolution/dcscn.py
+++ lmnet/lmnet/networks/super_resolution/dcscn.py
@@ -274,8 +274,8 @@ class Dcscn(BaseNetwork):
         base = tf.image.resize_images(base, (height, width), tf.image.ResizeMethod.BICUBIC)
         rgb_output = tf.py_func(self._combine_y_and_cbcr_to_rgb, [output, base], tf.float32)
 
-        # tf.summary.image("metrics/label", tf.cast(tf.clip_by_value(gt_images, 0, 255), tf.uint8))
-        # tf.summary.image("metrics/output", tf.cast(tf.clip_by_value(rgb_output, 0, 255), tf.uint8))
+        gt_images = tf.clip_by_value(gt_images, 0, 255)
+        rgb_output = tf.clip_by_value(rgb_output, 0, 255)
  
         # Calc metrics
         results = {}
