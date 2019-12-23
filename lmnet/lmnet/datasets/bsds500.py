36e32bc4cb3813836b98503f74cbc416218cf31e
suttang <suttang@gmail.com>
2019-12-23 18:29:28 +0900

    bsds500 dataset

---
+++ lmnet/lmnet/datasets/bsds500.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Blueoil Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+import functools
+import os
+import random
+from glob import glob
+
+from lmnet.datasets.base import Base
+from lmnet.datasets.base import SuperResolutionBase
+from lmnet.utils.image import crop, load_image, scale
+
+
+class Bsds500(Base):
+    classes = []
+    num_classes = 0
+    extend_dir = "BSDS500/data/images"
+    available_subsets = ["train", "validation"]
+
+    @property
+    @functools.lru_cache(maxsize=None)
+    def files(self):
+        if self.subset == "train":
+            images_dir = os.path.join(self.data_dir, "train")
+        else:
+            images_dir = os.path.join(self.data_dir, "val")
+
+        return [filepath for filepath in glob(os.path.join(images_dir, "*.jpg"))]
+
+    @property
+    def num_per_epoch(self):
+        return len(self.files)
+
+    def __getitem__(self, i, type=None):
+        target_file = self.files[i]
+        image = load_image(target_file)
+
+        return image, None
+
+    def __len__(self):
+        return self.num_per_epoch
+
+
+class Bsds500SuperResolution(SuperResolutionBase):
+    classes = Bsds500.classes
+    num_classes = Bsds500.num_classes
+    extend_dir = Bsds500.extend_dir
+    available_subsets = Bsds500.available_subsets
+
+    def __init__(self, scale, patch_size, **kwargs):
+        self.dataset = Bsds500(**kwargs)
+
+        self.scale = scale
+        self.patch_size = patch_size
+
+        self.subset = self.dataset.subset
+        self.batch_size = self.dataset.batch_size
+        self.augmentor = self.dataset.augmentor
+        self.pre_processor = self.dataset.pre_processor
+        self.data_format = self.dataset.data_format
+        self.seed = self.dataset.seed
+
+    @property
+    def files(self):
+        return self.dataset.files
+    
+    @property
+    def num_per_epoch(self):
+        return self.dataset.num_per_epoch
+    
+    def __getitem__(self, i):
+        image, _ = self.dataset[i]
+
+        if self.subset == "train":
+            cropped_image = crop(image, self.patch_size)
+            image = scale(cropped_image, 1 / self.scale)
+            label = cropped_image
+        elif self.subset == "validation":
+            # label = image
+            # image = scale(image, 1 / self.scale)
+            cropped_image = crop(image, 300)
+            image = scale(cropped_image, 1 / self.scale)
+            label = cropped_image
+        return image, label
+    
+    def __len__(self):
+        return len(self.dataset)
