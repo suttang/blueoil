ec4482b1a157246385537d6efb3b2cc7ced8c3ac
suttang <suttang@gmail.com>
2019-12-27 12:00:09 +0900

    change lr

--- lmnet/configs/core/super_resolution/dcscn.py
+++ lmnet/configs/core/super_resolution/dcscn.py
@@ -70,7 +70,7 @@ NETWORK.OPTIMIZER_CLASS = tf.train.AdamOptimizer
 NETWORK.OPTIMIZER_KWARGS = {'beta1': 0.9, 'beta2': 0.999}
 NETWORK.LEARNING_RATE_FUNC = tf.train.cosine_decay
 NETWORK.LEARNING_RATE_KWARGS = {
-    "learning_rate": 0.0005,
+    "learning_rate": 0.0003,
     "decay_steps": 80 * 800,
 }
 NETWORK.IMAGE_SIZE = IMAGE_SIZE
